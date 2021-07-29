a369db7511a6075e305625a0bb38b00995a80568
lukasrvs <lucas_antonio.v@hotmail.com>
2021-07-28 21:14:02 -0300

    testing spreadsheets

---
+++ server.js
@@ -0,0 +1,43 @@
+const x1 = require("excel4node");
+const wb = new x1.Workbook();
+const ws = wb.addWorksheet("Nome da planilha");
+
+const data = [
+    {
+        "name": "abc",
+        "email": "abc@email.com",
+        "phone": "123"
+    },
+    {
+        "name": "def",
+        "email": "def@email.com",
+        "phone": "456"
+    },
+    {
+        "name": "ghi",
+        "email": "ghi@email.com",
+        "phone": "789"
+    }
+];
+
+const headColumnsTitles = [
+    "Name",
+    "E-mail",
+    "Phone"
+];
+
+let headColumnsIndex=1;
+headColumnsTitles.forEach(heading=>{
+    ws.cell(1, headColumnsIndex++).string(heading);
+});
+
+let rowIndex=2;
+data.forEach(record=>{
+    let columnIndex=1;
+    Object.keys(record).forEach(columnName=>{
+        ws.cell(rowIndex,columnIndex++).string(record[columnName]);
+    });
+    rowIndex++;
+});
+
+wb.write("arquivo.xlxs");
